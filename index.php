015322029b3ed3ccd5da892dc516db15c20cac6e
unknown <carltan2700@gmail.com>
2024-01-27 22:23:07 +0800

    just started a new php activiy

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Money Button Game</title>
+        <style>
+            main{
+                outline: 1px solid black;
+                width: 80%;
+                margin: 0 auto;
+                padding: 2%;
+                text-align: center;
+            }
+            header h2, input{
+                display: inline-block;
+            }
+            header h2{
+                margin-right: 64%;
+            }
+            header input{
+                background-color: red;
+                color: white;
+            }
+            form{
+                outline: 3px solid black;
+                display: inline-block;
+                margin: 2%;
+                width: 20%;
+                min-height: 10%;
+            }
+            form input{
+                background-color: green;
+                color: white;
+                padding: 1% 4%;
+            }
+            h3{
+                text-align: left;
+                margin-bottom: 1%;
+            }
+            #gamehost-container{
+                width: 93%;
+                margin: 0 auto;
+            }
+            #gamehost-container div{
+                outline: 1px solid black;
+                height: 200px;
+                max-height: 200px; 
+                overflow: scroll;
+                overflow-x: hidden;
+            }
+        </style>
+    </head>
+    <body>
+        <main>
+            <header>
+                <h2>Your Money: 'Value'</h2>
+                <input type="submit" value='Reset Game' id='reset'>
+            </header>
+            <div>
+                <form action="process.php" method='post'>
+                    <input type="hidden" name='button' value='low'>
+                    <h2>Low Risk</h2>
+                    <input type="submit" value='Bet'>
+                    <p>by -25 up to 100</p>
+                </form>
+                <form action="process.php" method='post'>
+                    <input type="hidden" name='button' value='moderate'>
+                    <h2>Moderate Risk</h2>
+                    <input type="submit" value='Bet'>
+                    <p>by -100 up to 1000</p>
+                </form>
+                <form action="process.php" method='post'>
+                    <input type="hidden" name='button' value='high'>
+                    <h2>High Risk</h2>
+                    <input type="submit" value='Bet'>
+                    <p>by -500 up to 2500</p>
+                </form>
+                <form action="process.php" method='post'>
+                    <input type="hidden" name='button' value='severe'>
+                    <h2>Severe Risk</h2>
+                    <input type="submit" value='Bet'>
+                    <p>by -3000 up to 5000</p>
+                </form>
+            </div>
+            <div id='gamehost-container'>
+                <h3>Game Host:</h3>
+                <div>
+                    it's a box
+                    <?php echo "nothing yet to see" ?>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
